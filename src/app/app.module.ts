51e12c020ceee86fa05092da3fa165ccf5a057ad
kyleconkright <kyleconkright@gmail.com>
2017-05-20 19:43:44 -0700

    added reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { JobsListComponent } from './jobs-list/jobs-list.component';
+import { JobComponent } from './job/job.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    JobsListComponent
+    JobsListComponent,
+    JobComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     JsonpModule,
     HttpModule
