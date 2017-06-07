d18e998720a79fcd4d59b6660905a878504d381d
Daniel Dowling <ddowling30@gmail.com>
2017-06-07 13:22:07 -0700

    heroku deploy service update

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ app.get('/indeed/',(req, response) => {
 
 })
 
- var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+//  var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
 
-// app.listen(process.env.PORT || 8080), () => {
-//     console.log('server running..')
-// }
+app.listen(process.env.PORT || 8080), () => {
+    console.log('server running..')
+}
 
 module.exports = app;
