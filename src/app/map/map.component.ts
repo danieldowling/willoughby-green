bf4ba7121baa2c073e6ac21f2b4516885fb7092b
Daniel Dowling <ddowling30@gmail.com>
2017-06-10 15:26:47 -0700

    added geolocation on page load

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,9 +10,10 @@ import { AgmCoreModule, MapsAPILoader } from 'angular2-google-maps/core';
   styleUrls: ['./map.component.scss']
 })
 
-export class MapComponent implements OnChanges {
- lat = 34.020479
- lng = -118.4117325
+export class MapComponent implements OnInit {
+ lat = 100  
+ lng = 100
+
   constructor(private searchService: SearchBarService) {
     //access values saved from searchService, set lat and lng of map equal each time lat/lng changes in searchService
     this.searchService.lat$.subscribe(
@@ -24,15 +25,25 @@ export class MapComponent implements OnChanges {
         this.lng = updatedLng
       }
     )
-    //console.log(this.lat)
-    //console.log(this.lng)
-    //console.log('hello')
+    
   }
 
-  ngOnChanges() {
-    // this.lat = this.searchService.lat
-    // this.lng = this.searchService.lng
+  ngOnInit() {
+    // verify the browser has geolocation
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+    } else {
+      /* geolocation IS NOT available */
+    }
+  
+  //gets users current location, sets lat/lng equal to their location on init
+  navigator.geolocation.getCurrentPosition((position) => {
+    this.lat = position.coords.latitude
+    this.lng = position.coords.longitude
+  });
+    
+}
 
-  }
 
+  
 }
