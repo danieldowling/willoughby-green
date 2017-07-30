7d26b409edbd9a084bb1b826f1cb3177a5795b8d
Daniel Dowling <ddowling30@gmail.com>
2017-07-30 22:12:49 +0100

    added cors to http function for firebase

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var http = require('http');
 var https = require('https');
-var parser = require('xml2json')
+var parser = require('xml2json');
 
 exports.indeed = functions.https.onRequest((req, res) =>{
     app.use(bodyParser.json());
@@ -12,20 +12,19 @@ exports.indeed = functions.https.onRequest((req, res) =>{
 
     app.use(express.static(__dirname + '/dist'));
 
-    app.use(function (req, res, next) {
+    cors(req, res, () => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        next();
     });
 
     app.get("/indeed/", (req, response) => {
         request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
         res.send(parser.toJson(body))
-  
+
     })
 });
 
  module.exports = app;
-    
+
 
 })
