cbd544a63f03edc81d7b0eba265b3adc32d0c3f7
Daniel Dowling <ddowling30@gmail.com>
2017-08-06 12:57:14 +0100

    CORS firebase function

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,32 @@ const functions = require('firebase-functions');
 var app = express();
 var http = require('http');
 var https = require('https');
-const cors = require('cors') ({
-  origin: true
-});
+var cors = require('cors');
 
 
-exports.indeed = functions.https.onRequest((req, res) =>{
-  app.use(cors((req, res, () => {
-      app.get("/indeed/", (req, response) => {
-        request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
-        res.send(body)
-      })
-    });
-  })))
+var jobFn = function jobFn(req, res) {
+  res.status(200)
+    request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
+      res.send(body)
+    })
+};
+
+//CORS & CloudFunction
+exports.indeed = functions.https.indeed((req, res) => {
+  var corsFn = cors();
+  corsFn(req, res, function() {
+    jobFn(req, res);
+  })
 })
+
+
+
+// exports.indeed = functions.https.onRequest((req, res) =>{
+//   app.use(cors((req, res, () => {
+//       app.get("/indeed/", (req, response) => {
+//         request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
+//         res.send(body)
+//       })
+//     });
+//   })))
+// })
