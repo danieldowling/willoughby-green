c01b9cf31f150c3118b2dd7e54ab063e2b446c96
Daniel Dowling <ddowling30@gmail.com>
2017-08-06 12:20:56 +0100

    updated cors syntax for firebase

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,19 @@
 var express = require('express')
 const functions = require('firebase-functions');
 var app = express();
-var bodyParser = require('body-parser');
 var http = require('http');
 var https = require('https');
-var parser = require('xml2json');
-
-exports.indeed = functions.https.onRequest((req, res) =>{
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-
-    app.use(express.static(__dirname + '/dist'));
-
-    cors(req, res, () => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    });
-
-    app.get("/indeed/", (req, response) => {
-        request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
-        res.send(parser.toJson(body))
-
-    })
+const cors = require('cors') ({
+  origin: true
 });
 
- module.exports = app;
 
-
+exports.indeed = functions.https.onRequest((req, res) =>{
+  app.use(cors((req, res, () => {
+      app.get("/indeed/", (req, response) => {
+        request(`http://api.indeed.com/ads/apisearch?publisher=${functions.config().indeed.key}&q=${req.query.keyword}&l=${req.query.location}&sort=&radius=&st=&jt=&start=&limit=100&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Chrome/31.0.1650.63&v=2`, (err, res, body) => {
+        res.send(body)
+      })
+    });
+  })))
 })
--- src/app/search-bar/search-bar.service.ts
+++ src/app/search-bar/search-bar.service.ts
@@ -1,32 +1,32 @@
-import { Http, Response, URLSearchParams } from "@angular/http";
-import { Injectable, Output, EventEmitter } from "@angular/core";
+import { Http, Response, URLSearchParams } from '@angular/http';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import 'rxjs/Rx';
-import { Subject } from "rxjs/Subject";
-import { environment } from "../../environments/environment";
+import { Subject } from 'rxjs/Subject';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class SearchBarService {
 
 
-  location: string = "";
-  keyword: string = "";
+  location: string = '';
+  keyword: string = '';
 
   constructor (private http: Http) {}
-  //set variables equal to new behavior subject
+  // set variables equal to new behavior subject
   private lat = new Subject<any>();
   private lng = new Subject<any>();
-  //create observables
+  // create observables
   lat$ = this.lat.asObservable();
-  lng$ = this.lng.asObservable(); 
+  lng$ = this.lng.asObservable();
   jobs: Object[] = [];
-  
+
   searchLocation(searchData) {
     let params = new URLSearchParams();
     params.set('location', searchData.location);
-    params.set('keyword', searchData.keyword); 
+    params.set('keyword', searchData.keyword);
     return this.http.get(`https://us-central1-jobwalkerpro.cloudfunctions.net/indeed/`, { search: params })
       .subscribe(res => {
-        //set value of jobs, lat, and lng
+        // set value of jobs, lat, and lng
         console.log(res)
         this.jobs = res.json().response.results.result
         this.lat.next(res.json().response.results.result[0].latitude)
@@ -34,6 +34,5 @@ export class SearchBarService {
         console.log(this.lat, this.lng, this.jobs)
     })
   }
-  
+
 }
-       
